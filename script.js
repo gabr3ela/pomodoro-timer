787730cdacd2cc0762806c5e6d95db0528160b24
Gabriela Corrêa <gabrielacorrea18706@gmail.com>
2026-02-06 16:13:28 -0300

    Add files via upload
    Projeto Pomodoro Timer desenvolvido com HTML, CSS e JavaScript para praticar manipulação de tempo, interface e organização de layout.

---
+++ script.js
@@ -0,0 +1,52 @@
+let minutes = 25;
+let seconds = 0;
+let interval = null;
+
+
+const displayMinutes = document.getElementById("minutes");
+const displaySeconds = document.getElementById("seconds");
+
+function updateDisplay() {
+    displayMinutes.textContent = minutes < 10 ? "0" + minutes : minutes;
+    displaySeconds.textContent = seconds < 10 ? "0" + seconds : seconds;
+}
+
+function timerLogic() {
+    if (seconds === 0) {
+        if (minutes === 0) {
+            clearInterval(interval); 
+            interval = null;
+            alert("Tempo acabou! Hora de descansar.");
+            return;
+        }
+        minutes--;
+        seconds = 59;
+    } else {
+        seconds--;
+    }
+    
+    updateDisplay();
+}
+
+
+document.getElementById("start").addEventListener("click", function(){
+    if (interval === null) {
+        interval = setInterval(timerLogic, 1000);
+    }
+});
+
+
+document.getElementById("pause").addEventListener("click", function(){
+    clearInterval(interval);
+    interval = null; 
+});
+
+
+document.getElementById("reset").addEventListener("click", function(){
+    clearInterval(interval);
+    interval = null;
+    minutes = 25;
+    seconds = 0;
+    
+    updateDisplay();
+});
